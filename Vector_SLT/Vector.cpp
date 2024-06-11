c4f5aeea2313d9a4944a98d52533909fd7a08129
Asif Ahammad <asifahammed359@gmail.com>
2024-06-11 18:32:00 +0600

    add some slt function

--- Vector_SLT/Vector.cpp
+++ Vector_SLT/Vector.cpp
@@ -48,12 +48,183 @@ void capacity(void)
 
 }
 
+// vector modifiers 
+void modifiers(void)
+{
+    vector<int> x = {10,20,30};
+    vector<int> v = {1,2,3};
+
+    v=x ; // O(N)
+    
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    v.pop_back();
+    v.pop_back();
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    
+
+}
+
+//VectorInsert
+void VectorInsert(void)
+{
+    vector <int> V = {1,2,3,4,5};
+    vector <int> V2 = {10,20,30};
+    V.insert(V.begin()+V.size(),V2.begin(),V2.end());
+
+    for ( int x : V)
+    {
+        cout <<x<< " ";
+    }
+    cout<< endl;
+    
+}
+
+//VectorErese
+void VectorErese(void)
+{
+    vector <int> V = {1,2,3,4,5};
+    vector <int> V2 = {10,20,30};
+    V.insert(V.begin()+V.size(),V2.begin(),V2.end());
+
+    V.erase(V.begin()+2, V.begin()+4); // erese 1+2=3 index;
+
+    for ( int x : V)
+    {
+        cout <<x<< " ";
+    }
+    cout<< endl;
+    
+}
+
+
+//vectorreplace
+void vectorreplace(void)
+{
+    vector <int> V = {1,2,3,4,5};
+    vector <int> V2 = {6,7,8};
+    V.insert(V.begin()+V.size(),V2.begin(),V2.end());
+
+    
+
+    for ( int x : V)
+    {
+        if(x % 2 == 0)
+        {
+            replace(V.begin(),V.end(),x,0);
+        }
+        else
+        {
+            replace(V.begin(),V.end(),x,1);
+        }
+        
+    }
+    cout<< endl;
+
+    // Printing the vector
+    for (int x : V)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+}
+
+
+//vectorFind
+void vectorFind(void)
+{
+    vector <int> V = {1,6,3,9,5};
+    vector <int> V2 = {6,7,8};
+    V.insert(V.begin()+V.size(),V2.begin(),V2.end());
+
+    // vector<int> :: iterator it;
+
+    auto it = find(V.begin(),V.end(),2);
+    // cout << *it << endl;
+    if(it == V.end())
+    {
+        cout << " Not Found \n";
+    }
+    else
+    {
+        cout << "Found \n";
+    }
+}
+
+
+
+//vectorAccess
+void vectorAccess(void)
+{
+    vector <int> V = {1,6,3,9,5};
+    vector <int> V2 = {6,7,8};
+    V.insert(V.begin()+V.size(),V2.begin(),V2.end());
+
+    // cout << V.back() << " \n";
+    // cout << V.front()<< " \n";
+
+    vector<int> :: iterator it;
+
+    for (it = V.begin(); it < V.end(); it++)
+    {
+        cout<< *it << " ";
+    }
+    cout<<"\n";
+}
+
+
+
+//vectorInput
+void vectorInput(void)
+{
+    int N ; 
+    cout << "Enter Vector Size : ";
+    cin >> N;
+
+    vector<int>V;
+    // vector<int>V(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        int item ;
+        cin >> item;
+        V.push_back(item);
+        // cin >> V[i];
+
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << V[i] << " ";
+    }
+    cout << "\n";
+    
+}
 
 int main()
 {
     
     // initialization();
-    capacity();
+    // capacity();
+    // modifiers();
+    // VectorInsert();
+    // VectorErese();
+    // vectorreplace();
+    // vectorFind();
+    // vectorAccess();
+
+    vectorInput();
 
     return 0;
 }
