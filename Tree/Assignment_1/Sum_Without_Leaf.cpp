4abe5c7b43ee769b481926c46f7efe831ecd7b64
Asif3359 <asifahammed359@gmail.com>
2024-08-22 22:47:42 +0600

    solve sum without leaf

---
+++ Tree/Assignment_1/Sum_Without_Leaf.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int Val;
+    Node *left;
+    Node *right;
+
+    Node(int Val)
+    {
+        this->Val = Val;
+        this->left = NULL;
+        this->right = NULL; 
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+
+    Node *root;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+    queue<Node *> q;
+
+    if (root)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        // 1
+        Node *p = q.front();
+        q.pop();
+
+        // 2
+        int l, r;
+        cin >> l >> r;
+
+        Node *left, *right;
+
+        if (l == -1)
+        {
+            left = NULL;
+        }
+        else
+        {
+            left = new Node(l);
+        }
+        if (r == -1)
+        {
+            right = NULL;
+        }
+        else
+        {
+            right = new Node(r);
+        }
+
+        p->left = left;
+        p->right = right;
+
+        // 3
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+
+    return root;
+}
+
+int  sumWithOutLeafNode (Node * root)
+{
+    if(root == NULL)
+    {
+        return 0 ;
+    }
+
+    if(root->left == NULL && root->right == NULL) 
+    {
+        return 0 ;
+    }
+
+    int l = sumWithOutLeafNode(root->left);
+    int r = sumWithOutLeafNode(root->right);
+
+    return l + r + root->Val;
+}
+
+int main() 
+{
+    Node *root = input_tree();
+    cout << sumWithOutLeafNode(root) << "\n";
+    return 0;
+}
